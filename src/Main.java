0eb46972ba91787dc21174df7b0ed695e2c5cd3e
blakebt <brenden.blake94@gmail.com>
2025-06-17 18:35:41 -0400

    Set up ExecutorService with FixedThreadPool to simulate multiple concurrent transactions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
+        Bank bank = new Bank();
+        Account account = new Account("Bob", 1000.0);
 
+        try(ExecutorService exec = Executors.newFixedThreadPool(3)) {
+            exec.execute(new HolderTask(account, account.getAccountName(), 100, Task.DEPOSIT));
+            bank.getTransactionLog().add(new Transaction(
+                    bank.getTransactionLog().size() + 1,
+                    account.getAccountName(),
+                    Task.DEPOSIT,
+                    100)
+            );
+            exec.execute(new HolderTask(account, account.getAccountName(), 200, Task.WITHDRAW));
+            bank.getTransactionLog().add(new Transaction(
+                    bank.getTransactionLog().size() + 1,
+                    account.getAccountName(),
+                    Task.WITHDRAW,
+                    200)
+            );
+            exec.execute(new HolderTask(account, account.getAccountName(), 100, Task.DEPOSIT));
+            bank.getTransactionLog().add(new Transaction(
+                    bank.getTransactionLog().size() + 1,
+                    account.getAccountName(),
+                    Task.DEPOSIT,
+                    100)
+            );
+            exec.execute(new HolderTask(account, account.getAccountName(), 100, Task.DEPOSIT));
+            bank.getTransactionLog().add(new Transaction(
+                    bank.getTransactionLog().size() + 1,
+                    account.getAccountName(),
+                    Task.DEPOSIT,
+                    100)
+            );
+            exec.execute(new HolderTask(account, account.getAccountName(), 300, Task.WITHDRAW));
+            bank.getTransactionLog().add(new Transaction(
+                    bank.getTransactionLog().size() + 1,
+                    account.getAccountName(),
+                    Task.WITHDRAW,
+                    300)
+            );
+
+            exec.shutdown();
+        }
+
+        System.out.println();
+        System.out.println(account.getBalance());
+        bank.getTransactionLog().forEach(System.out::println);
     }
 }
