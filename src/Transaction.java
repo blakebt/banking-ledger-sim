f96902fdb65cc1293bda925885f4576c9c58f9fe
blakebt <brenden.blake94@gmail.com>
2025-06-18 15:17:25 -0400

    Updated class to include a sourceAccount field for transfer operations. Updated toString() to account for transfer operations.

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,21 +1,40 @@
 import enums.BankOperation;
+import model.Account;
 
 import java.time.ZonedDateTime;
 
 public class Transaction {
     private final long transactionId;
-    private final String destinationAccount;
+    private final Account sourceAccount;
+    private final Account destinationAccount;
     private final BankOperation bankOperation;
     private final double amount;
     private final ZonedDateTime timeOfTransaction;
 
     public Transaction(
             long transactionId,
-            String destinationAccount,
+            Account sourceAccount,
+            Account destinationAccount,
             BankOperation bankOperation,
             double amount
     ) {
         this.transactionId = transactionId;
+        this.sourceAccount = sourceAccount;
+        this.destinationAccount = destinationAccount;
+        this.bankOperation = bankOperation;
+        this.amount = amount;
+        this.timeOfTransaction = ZonedDateTime.now();
+
+    }
+
+    public Transaction(
+            long transactionId,
+            Account destinationAccount,
+            BankOperation bankOperation,
+            double amount
+    ) {
+        this.transactionId = transactionId;
+        this.sourceAccount = null;
         this.destinationAccount = destinationAccount;
         this.bankOperation = bankOperation;
         this.amount = amount;
@@ -24,10 +43,32 @@ public class Transaction {
 
     @Override
     public String toString() {
-        if(bankOperation == BankOperation.DEPOSIT) {
-            return String.format("%d - %s deposited %.2f at %s", transactionId, destinationAccount, amount, timeOfTransaction);
-        } else {
-            return String.format("%d - %s withdrew %.2f at %s", transactionId, destinationAccount, amount, timeOfTransaction);
+        switch(bankOperation) {
+            case BankOperation.DEPOSIT -> {
+                return String.format("%d - %s deposited $%.2f at %s",
+                        transactionId,
+                        destinationAccount.getAccountName(),
+                        amount,
+                        timeOfTransaction);
+            }
+            case BankOperation.WITHDRAW -> {
+                return String.format("%d - %s withdrew $%.2f at %s",
+                        transactionId,
+                        destinationAccount.getAccountName(),
+                        amount,
+                        timeOfTransaction);
+            }
+            case BankOperation.TRANSFER -> {
+                return String.format("%d - %s transferred $%.2f to %s at %s",
+                        transactionId,
+                        sourceAccount.getAccountName(),
+                        amount,
+                        destinationAccount.getAccountName(),
+                        timeOfTransaction);
+            }
+            default -> {
+                return "Invalid Bank Operation"; // should never get here
+            }
         }
     }
 }
