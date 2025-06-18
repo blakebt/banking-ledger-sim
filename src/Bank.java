2b92a74d5bf9c80f3642bd9601a0283b74873f56
blakebt <brenden.blake94@gmail.com>
2025-06-18 11:41:14 -0400

    Refactored to have the ExecutorService injected and to adjust for other changes. Separated transaction and logging logic.

--- src/Bank.java
+++ src/Bank.java
@@ -1,15 +1,50 @@
+import enums.BankOperation;
+import model.Account;
+import operation.Operation;
+import operation.OperationFactory;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 public class Bank {
-    private List<Account> accounts;
-    private List<Transaction> transactionLog;
+    private final List<Account> accounts;
+    private final List<Transaction> transactionLog;
+    private final ExecutorService exec;
 
-    public Bank() {
-        accounts = new ArrayList<>();
-        transactionLog = new ArrayList<>();
+    public Bank(ExecutorService exec) {
+        this.accounts = new ArrayList<>();
+        this.transactionLog = new ArrayList<>();
+        this.exec = exec;
     }
 
     public List<Account> getAccounts() { return accounts; }
     public List<Transaction> getTransactionLog() { return transactionLog; }
+
+    public void completeTransaction(Account account, BankOperation bankOperation, double amount) {
+        Operation operation = OperationFactory.createBasicOperation(bankOperation, account ,amount);
+        // perform the transaction
+        exec.execute(operation);
+        logTransaction(account, bankOperation, amount);
+
+    }
+
+    public void completeTransaction(
+            Account source,
+            Account destination,
+            BankOperation bankOperation,
+            double amount
+    ) {
+        Operation operation = OperationFactory.createTransferOperation(source, destination, amount);
+        exec.execute(operation);
+    }
+
+    private void logTransaction(Account account, BankOperation bankOperation, double amount) {
+        transactionLog.add(new Transaction(
+                transactionLog.size() + 1,
+                account.getAccountName(),
+                bankOperation,
+                amount)
+        );
+    }
 }
