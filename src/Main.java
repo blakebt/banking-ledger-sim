32672bba7ee3382970297b560882905694dec422
blakebt <brenden.blake94@gmail.com>
2025-06-18 15:16:03 -0400

    Adjusted code to test new transfer feature

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,26 @@ import java.util.concurrent.Executors;
 public class Main {
     public static void main(String[] args) {
         Bank bank;
-        Account account = new Account("Bob", 1000.0);
-        Account destinationAccount = new Account("Alice", 1500.0);
+        Account bobAccount = new Account("Bob", 1000.0);
+        Account aliceAccount = new Account("Alice", 1500.0);
 
         try(ExecutorService exec = Executors.newFixedThreadPool(3)) {
             bank = new Bank(exec);
-            bank.completeTransaction(account, BankOperation.DEPOSIT, 100);
-            bank.completeTransaction(account, BankOperation.DEPOSIT, 200);
-            bank.completeTransaction(account, BankOperation.WITHDRAW, 100);
-            bank.completeTransaction(account, BankOperation.DEPOSIT, 300);
-            bank.completeTransaction(account, BankOperation.WITHDRAW, 500);
+            bank.completeTransaction(bobAccount, BankOperation.DEPOSIT, 100);
+            bank.completeTransaction(bobAccount, BankOperation.DEPOSIT, 200);
+            bank.completeTransaction(bobAccount, BankOperation.WITHDRAW, 100);
+            bank.completeTransaction(bobAccount, BankOperation.DEPOSIT, 300.50);
+            bank.completeTransaction(aliceAccount, bobAccount, BankOperation.TRANSFER, 100);
+            bank.completeTransaction(bobAccount, BankOperation.WITHDRAW, 501.25);
+            bank.completeTransaction(aliceAccount, BankOperation.DEPOSIT, 105.45);
+            bank.completeTransaction(aliceAccount, bobAccount, BankOperation.TRANSFER, 100);
 
             exec.shutdown();
         }
 
         System.out.println();
-        System.out.println(account.getBalance());
+        System.out.println(bobAccount.getBalance());
+        System.out.println(aliceAccount.getBalance());
         bank.getTransactionLog().forEach(System.out::println);
     }
 }
