f599ef0e0938d6a599b0a07d6d61e06e7dc83b63
blakebt <brenden.blake94@gmail.com>
2025-06-18 11:38:51 -0400

    Implemented an OperationFactory to create the Operations

---
+++ src/operation/OperationFactory.java
@@ -0,0 +1,19 @@
+package operation;
+
+import enums.BankOperation;
+import model.Account;
+
+public class OperationFactory {
+
+    public static Operation createBasicOperation(BankOperation type, Account account, double amount) {
+        if(type == BankOperation.DEPOSIT) {
+            return new DepositOperation(account, amount);
+        } else {
+            return new WithdrawOperation(account, amount);
+        }
+    }
+
+    public static Operation createTransferOperation(Account source, Account destination, double amount) {
+        return new TransferOperation(source, destination, amount);
+    }
+}
