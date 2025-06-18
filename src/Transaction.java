c66b341beb203dc1919c60425c45b30f9ef6a3e3
blakebt <brenden.blake94@gmail.com>
2025-06-18 11:39:13 -0400

    Renaming due to the change in name from Task to BankOperation

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,28 +1,30 @@
+import enums.BankOperation;
+
 import java.time.ZonedDateTime;
 
 public class Transaction {
     private final long transactionId;
     private final String destinationAccount;
-    private final Task task;
+    private final BankOperation bankOperation;
     private final double amount;
     private final ZonedDateTime timeOfTransaction;
 
     public Transaction(
             long transactionId,
             String destinationAccount,
-            Task task,
+            BankOperation bankOperation,
             double amount
     ) {
         this.transactionId = transactionId;
         this.destinationAccount = destinationAccount;
-        this.task = task;
+        this.bankOperation = bankOperation;
         this.amount = amount;
         this.timeOfTransaction = ZonedDateTime.now();
     }
 
     @Override
     public String toString() {
-        if(task == Task.DEPOSIT) {
+        if(bankOperation == BankOperation.DEPOSIT) {
             return String.format("%d - %s deposited %.2f at %s", transactionId, destinationAccount, amount, timeOfTransaction);
         } else {
             return String.format("%d - %s withdrew %.2f at %s", transactionId, destinationAccount, amount, timeOfTransaction);
