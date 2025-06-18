89e71a5febf491e0e40a2995dd37bd748b758712
blakebt <brenden.blake94@gmail.com>
2025-06-17 20:33:51 -0400

    Adjusted HolderTask initialization due to a refactor in the class

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,35 @@ public class Main {
         Account account = new Account("Bob", 1000.0);
 
         try(ExecutorService exec = Executors.newFixedThreadPool(3)) {
-            exec.execute(new HolderTask(account, account.getAccountName(), 100, Task.DEPOSIT));
+            exec.execute(new HolderTask(account, 100, Task.DEPOSIT));
             bank.getTransactionLog().add(new Transaction(
                     bank.getTransactionLog().size() + 1,
                     account.getAccountName(),
                     Task.DEPOSIT,
                     100)
             );
-            exec.execute(new HolderTask(account, account.getAccountName(), 200, Task.WITHDRAW));
+            exec.execute(new HolderTask(account, 200, Task.WITHDRAW));
             bank.getTransactionLog().add(new Transaction(
                     bank.getTransactionLog().size() + 1,
                     account.getAccountName(),
                     Task.WITHDRAW,
                     200)
             );
-            exec.execute(new HolderTask(account, account.getAccountName(), 100, Task.DEPOSIT));
+            exec.execute(new HolderTask(account, 100, Task.DEPOSIT));
             bank.getTransactionLog().add(new Transaction(
                     bank.getTransactionLog().size() + 1,
                     account.getAccountName(),
                     Task.DEPOSIT,
                     100)
             );
-            exec.execute(new HolderTask(account, account.getAccountName(), 100, Task.DEPOSIT));
+            exec.execute(new HolderTask(account, 100, Task.DEPOSIT));
             bank.getTransactionLog().add(new Transaction(
                     bank.getTransactionLog().size() + 1,
                     account.getAccountName(),
                     Task.DEPOSIT,
                     100)
             );
-            exec.execute(new HolderTask(account, account.getAccountName(), 300, Task.WITHDRAW));
+            exec.execute(new HolderTask(account, 300, Task.WITHDRAW));
             bank.getTransactionLog().add(new Transaction(
                     bank.getTransactionLog().size() + 1,
                     account.getAccountName(),
