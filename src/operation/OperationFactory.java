d66058298b8033a9411c442a8a151911903e4a99
blakebt <brenden.blake94@gmail.com>
2025-06-19 12:34:22 -0400

    Added error handling for newBasicOperation in case it is called with an operation other than deposit or withdraw

--- src/operation/OperationFactory.java
+++ src/operation/OperationFactory.java
@@ -5,15 +5,31 @@ import model.Account;
 
 public class OperationFactory {
 
-    public static Operation newBasicOperation(BankOperation type, Account account, double amount) {
+    public static Operation newBasicOperation(
+            BankOperation type,
+            Account account,
+            double amount
+    ) throws IllegalArgumentException {
+
         if(type == BankOperation.DEPOSIT) {
             return new DepositOperation(account, amount);
-        } else {
+        }
+        if(type == BankOperation.WITHDRAW) {
             return new WithdrawOperation(account, amount);
         }
+
+        throw new IllegalArgumentException("Invalid BankOperation.");
     }
 
-    public static Operation newTransferOperation(Account source, Account destination, double amount) {
+    public static Operation newTransferOperation(
+            Account source,
+            Account destination,
+            double amount
+    ) {
+        if(source == null) {
+            throw new NullPointerException("Source account cannot be null.");
+        }
+
         return new TransferOperation(source, destination, amount);
     }
 }
